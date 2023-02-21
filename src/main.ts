d5abdf40ab0af3be1525bca11a67a3600f5bbc5d
petah <you@example.com>
2023-02-20 21:36:28 -0800

    Added grid library primeflex

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import './assets/main.css';
 import 'primevue/resources/themes/saga-blue/theme.css'; //theme
 import 'primevue/resources/primevue.min.css'; //core css
 import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.scss';
 
 const app = createApp(App);
 
